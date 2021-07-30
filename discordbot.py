107c7a5d4ad8a91fc5dcbe22b8b812959cbe2400
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-07-30 11:36:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,46 @@
-from discord.ext import commands
+
+import ast
+import asyncio
+import cv2
+from datetime import datetime, timedelta, timezone
+import math
 import os
+import random
+import re
+import signal
+import sys
 import traceback
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+import discord
+from discord.ext import tasks, commands
+import psutil
+import psycopg2, psycopg2.extras
+import traceback
 
+client = discord.Client(intents=discord.Intents.all())
+token = os.environ['TOKEN']
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+developer = client.get_user(827903603557007390)
 
+# 時間軸設定
+JST = timezone(timedelta(hours=+9), 'JST')
+    
+@tasks.loop(seconds=60)
+async def ch_edit_loop():
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('時間だよ')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@client.event
+async def on_ready():
+    ch_edit_loop.start()
+    print("Ready!!")
+    
+@client.event
+async def on_message(msg):
+    msg_ctt = msg.content
+    msg_ch = msg.channel
+    
+    if (msg_ctt.startswith('nyan!ping')):
+        await msg_ch.send('Nyang!!')
 
-
-bot.run(token)
+client.run(token)
