6555f1fd2dc5b289e6b9c6aed23c61e325e3cbb9
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-08-19 18:19:43 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -127,10 +127,10 @@ async def on_ready():
     print("Nyan!!")
     
 flag = True
+master_flag = True
 
 nyan_checking_members_id = []
 
-master_flag = True
     
 @client.event
 async def on_message(msg):
@@ -177,7 +177,7 @@ async def on_message(msg):
                 await msg_ch.send(f'{msg.author.mention}さんの黙れ申請を受理したにゃ')
                 if damare_count < 3:
                     # simulate something heavy
-                    await msg_ch.send(f'あと{3-len(damarer)}人でだまるにゃ…')
+                    await msg_ch.send(f'あと{3-damare_count)}人でだまるにゃ…')
                 elif damare_count == 3:
                     msg_list = (
                         'みんなそんなに黙ってほしーにゃか…',
