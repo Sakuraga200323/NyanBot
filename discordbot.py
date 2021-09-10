376ba112732333d88a508a117a255b689b3f0bd1
Sakuraga <66359410+Sakuraga200323@users.noreply.github.com>
2021-09-10 20:44:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -190,7 +190,7 @@ async def on_ready():
     
     nyan_ch = client.get_channel(870264545338347580)
     await log_ch.send('今めっちゃログ読んでるので待ってください(白目)')
-    msgs = [ msg for msg in await nyan_ch.history(limit=1000).flatten() if all(not msg.author.bot,msg.content!='')]
+    msgs = [ msg for msg in await nyan_ch.history(limit=1000).flatten() if all([not msg.author.bot,msg.content!=''])]
     for msg in msgs:
         num = 0
         for word in ng_word_tuple:
